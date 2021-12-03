3a04395b8cba41ce21ed4ffa10561d8816f82dd8
Russell Oyama <russelloyama@gmail.com>
2021-12-03 06:48:02 -1000

    bool to mark package status

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
+
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch!!!");
@@ -14,11 +16,13 @@ public class Delivery : MonoBehaviour
         if(other.tag == "Package")
         {
             Debug.Log("Package picked up");
+            hasPackage = true;
         }
 
-        if(other.tag == "Customer")
+        if(other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package delivered");
+            hasPackage = false;
         }
     }
 }
