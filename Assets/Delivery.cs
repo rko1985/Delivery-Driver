2507ea2eeadd9c31518b330d402bd06e0f8a5b62
Russell Oyama <russelloyama@gmail.com>
2021-12-07 07:26:27 -1000

    changing driver color depending on package delivery status

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1); //white color
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1); //white color
     [SerializeField] float destroyDelay = 0.5f;
 
     bool hasPackage;
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -18,14 +26,16 @@ public class Delivery : MonoBehaviour
     {
         if(other.tag == "Package" && !hasPackage)
         {
-            Debug.Log("Package picked up");
+            Debug.Log("Package picked up");            
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, destroyDelay);
         }
 
         if(other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package delivered");
+            spriteRenderer.color = noPackageColor;
             hasPackage = false;
         }
     }
--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
-{
-
+{    
     [SerializeField] float steerSpeed = 1f;
     [SerializeField] float moveSpeed = 0.01f;
 
